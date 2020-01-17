c35b5832b9a8ef40d7fb4223663e545a019927b8
Amaryllisw <zhongwenhao123745@gmail.com>
2020-01-17 21:56:43 +0800

    add pass time

--- jump7.py
+++ jump7.py
@@ -1,6 +1,9 @@
+count = 0
 for num in range(1, 101):
 	if num == 7 or num % 10 == 7 or num // 10 == 7:
 		num += 1
+		count += 1
 		continue
 	print(num)
+print("total pass {} time".format(count))
 		
